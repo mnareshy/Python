021885179267bc2f350d188a1713cfdb2b1fd651
Naresh M <mnaresh.y@gmai.com>
2018-01-16 10:16:35 +0530

    Basics update

---
+++ test12/Bascics.py
@@ -0,0 +1,41 @@
+import sys
+from getpass import _raw_input
+from _ast import If
+
+x = "sairam"
+
+sys.stdout.write(x)
+
+
+
+if True:
+    print("\nIts TRUE")
+    
+elif False:
+    print("ITs False")
+    
+a = 100
+b = 1000.0
+c = "sairam"
+
+print (a);
+print (b);
+print (c);
+
+print ("\n\n");
+
+d = e = f = 10
+print (d);
+print (e);
+print (f);
+
+print ("\n\n");
+
+g,h,i = 20,20.2,"sai"
+print (g);
+print (h);
+print (i);
+
+
+
+_raw_input("Press Any Key")
