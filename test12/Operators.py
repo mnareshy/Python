d46d314f1c1ae78a8dd92bf572512ed827f1f3a7
Naresh M <mnaresh.y@gmai.com>
2018-02-12 09:50:44 +0530

    Operators addition

---
+++ test12/Operators.py
@@ -0,0 +1,59 @@
+# 1 + 2 = 3 , 1,2 are operands and + is operator
+
+# Arithmetic Operators
+
+a = 12
+b = 2
+c = -11
+d = 13
+e = -2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+# Floored Division , considers until . , if negative number rounds to negative.
+
+print(d//b)
+print(c//b)
+print(d//e)
+print(c//e)
+
+# **************************************************************************************************
+
+# Comparison Operators 
+
+
+print(a == b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+print(a < b)
+print(a > b)
+
+
+# **************************************************************************************************
+
+# Assignment Operators 
+
+f = a + b
+f = f + a
+f += a
+f -= a 
+f *= a
+f /= a
+f **= a
+f //= a
+
+print(f)
+
+
+# **************************************************************************************************
+
+# Bitwise Operators 
+
+
+
